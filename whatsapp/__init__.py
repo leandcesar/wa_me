abd81a0172b97fef5f6e7c4baabaeacf2cef60ff
Leandro César <ccleandroc@gmail.com>
2022-12-22 23:51:15 -0300

    feat: import all in init file

--- whatsapp/__init__.py
+++ whatsapp/__init__.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+
+__title__ = "whatsapp-py"
 __author__ = "Leandro César Cassimiro"
 __email__ = "ccleandroc@gmail.com"
 __version__ = "0.1.0"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2022-present, Leandro César Cassimiro"
+
+import logging
+
+from .bot import Bot  # NOQA
+from .classes import enums, events, messages, responses  # NOQA
+from .context import Ctx  # NOQA
+from .core import errors  # NOQA
+from .core.client import Client  # NOQA
+from .core.errors import *  # NOQA
+from .core.http import Route, HTTPClient  # NOQA
+from .utils.converter import as_dict, from_dict  # NOQA
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
